3989db4d666d1c0fb3c8e81c0f8696170f4107c9
pijeon <iitzdany@gmail.com>
2026-02-20 20:52:25 +0100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -57,7 +57,11 @@ export default {
                                 <span class="stat-label">ID</span>
                                 <div class="id-copy-box">
                                     <span class="stat-value">{{ level.id }}</span>
-                                    <button class="copy-btn" @click="copyID(level.id)">📋</button>
+                                    <button class="copy-id" onclick="navigator.clipboard.writeText('{{id}}')">
+<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24" fill="currentColor">
+<path d="M16 1H4C2.9 1 2 1.9 2 3V15H4V3H16V1ZM19 5H8C6.9 5 6 5.9 6 7V21C6 22.1 6.9 23 8 23H19C20.1 23 21 22.1 21 21V7C21 5.9 20.1 5 19 5ZM19 21H8V7H19V21Z"/>
+</svg>
+</button>
                                 </div>
                             </div>
 
