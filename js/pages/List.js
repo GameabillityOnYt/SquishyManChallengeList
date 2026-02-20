e36c3fe7ac5b462910ae3e0399a2b4ffe41882e2
pijeon <iitzdany@gmail.com>
2026-02-20 23:19:12 +0100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -99,21 +99,20 @@ C20.1 23 21 22.1 21 21V7C21 5.9 20.1 5 19
 <h3>List Editors</h3>
 <ol class="editors">
 <li v-for="editor in editors">
-<img :src="\`/assets/\${editor.role}\${store.dark?'-dark':''}.svg\`">
-<span>{{editor.name}}</span>
-</li>
-</ol>
-<ol class="editors">
-<li v-for="editor in editors">
 <img :src="iconFor(editor.role)">
 <span>{{editor.name}}</span>
 </li>
 </ol>
 
 <h3>Submission Requirements</h3>
-<p>Achieved the record without using hacks</p>
-<p>Achieved the record on the listed level</p>
-<p>Clicks or audio required</p>
+<p>Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)</p>
+<p>Achieved the record on the level that is listed on the site - please check the level ID before you submit a record</p>
+<p>Have either source audio or clicks/taps in the video. Edited audio only does not count</p>
+<p>The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this</p>
+<p>The recording must also show the player hit the endwall, or the completion will be invalidated.</p>
+<p>Do not use secret routes or bug routes</p>
+<p>Do not use easy modes, only a record of the unmodified level qualifies</p>
+<p>Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level</p>
 </div>
 </div>
 
