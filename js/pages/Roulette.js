74873d241a56c6b8b8b89fa361ae9660cc2b59f3
pijeon <iitzdany@gmail.com>
2026-02-22 16:31:14 +0100

    Update Roulette.js

--- js/pages/Roulette.js
+++ js/pages/Roulette.js
@@ -27,7 +27,7 @@ export default {
                         <input type="checkbox" id="extended" value="Extended List" v-model="useExtendedList">
                         <label for="extended">Extended List</label>
                     </div>
-                    <Btn @click.native.prevent="onStart">{{ levels.length === 0 ? 'Start' : 'Restart'}}</Btn>
+                    <Btn @click.native.prevent="onStart">Start</Btn>
                 </form>
                 <div class="sidebar-panel sidebar-autosave">
                     <p class="type-label-md roulette-note">
