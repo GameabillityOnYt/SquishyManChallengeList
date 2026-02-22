5d4d880ffa64368a3cf12c08e93666b80e62457d
pijeon <iitzdany@gmail.com>
2026-02-22 16:50:07 +0100

    Create spinner.js

---
+++ js/components/spinner.js
@@ -0,0 +1,7 @@
+export default {
+    template: `
+        <div class="spinner-wrap" role="status" aria-live="polite" aria-label="Loading">
+            <div class="spinner"></div>
+        </div>
+    `,
+};
