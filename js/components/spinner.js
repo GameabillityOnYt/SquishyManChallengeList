7b3fe649cd490800cb2c9b33a5a93882095dde2e
pijeon <iitzdany@gmail.com>
2026-02-22 17:09:01 +0100

    Update spinner.js

--- js/components/spinner.js
+++ js/components/spinner.js
@@ -1,6 +1,6 @@
 export default {
     template: `
-        <div class="spinner-wrap" role="status" aria-live="polite" aria-label="Loading">
+        <div class="spinner-overlay" role="status" aria-live="polite" aria-label="Loading">
             <div class="spinner"></div>
         </div>
     `,
