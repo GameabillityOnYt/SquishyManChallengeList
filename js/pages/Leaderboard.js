7d7d4decd894de2933e7a1a4477741fc9aa66056
pijeon <iitzdany@gmail.com>
2026-03-04 19:35:37 +0100

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -54,6 +54,7 @@ export default {
                         <p v-if="entry.user === 'Sdrawkcab'" class="champion-text">Champion</p>
                         <p v-if="entry.user === 'Pijeon'" class="champion-text">Champion</p>
                         <p v-if="entry.user === 'Panda'" class="champion-text">Champion</p>
+                        <p v-if="entry.user === '0cta'" class="champion-text">Champion</p>
                         <h3>{{ entry.total }}</h3>
 
                         <h2 v-if="entry.verified.length > 0">Verified ({{ entry.verified.length}})</h2>
