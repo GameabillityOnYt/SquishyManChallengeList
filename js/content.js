bbaaf206ac4ebbe10a8830765268f1227a00d9d0
pijeon <iitzdany@gmail.com>
2026-03-09 15:27:02 +0100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -38,34 +38,36 @@ async function getCached(key, loader, shouldPersist = (value) => value != null) 
 }
 
 export async function fetchList() {
-    const listResult = await fetch(`${dir}/_list.json`, { cache: 'no-store' });
-    try {
-        const list = await listResult.json();
-        return await Promise.all(
-            list.map(async (path, rank) => {
-                const levelResult = await fetch(`${dir}/${path}.json`, { cache: 'no-store' });
-                try {
-                    const level = await levelResult.json();
-                    return [
-                        {
-                            ...level,
-                            path,
-                            records: level.records.sort(
-                                (a, b) => b.percent - a.percent,
-                            ),
-                        },
-                        null,
-                    ];
-                } catch {
-                    console.error(`Failed to load level #${rank + 1} ${path}.`);
-                    return [null, path];
-                }
-            }),
-        );
-    } catch {
-        console.error(`Failed to load list.`);
-        return null;
-    }
+    return getCached('list', async () => {
+        const listResult = await fetch(`${dir}/_list.json`);
+        try {
+            const list = await listResult.json();
+            return await Promise.all(
+                list.map(async (path, rank) => {
+                    const levelResult = await fetch(`${dir}/${path}.json`);
+                    try {
+                        const level = await levelResult.json();
+                        return [
+                            {
+                                ...level,
+                                path,
+                                records: level.records.sort(
+                                    (a, b) => b.percent - a.percent,
+                                ),
+                            },
+                            null,
+                        ];
+                    } catch {
+                        console.error(`Failed to load level #${rank + 1} ${path}.`);
+                        return [null, path];
+                    }
+                }),
+            );
+        } catch {
+            console.error(`Failed to load list.`);
+            return null;
+        }
+    });
 }
 
 export async function fetchEditors() {
