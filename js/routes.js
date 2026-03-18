e766e270e60f171675b840152bcd8e8540dea739
pijeon <iitzdany@gmail.com>
2026-03-18 21:18:54 +0100

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,11 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
+import Players from './pages/Players.js';
 import Roulette from './pages/Roulette.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
+    { path: '/players', component: Players },
     { path: '/roulette', component: Roulette },
 ];
