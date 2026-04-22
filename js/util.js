8776da6e371eb145bec3bbc1d1533ed168fb9698
pijeon <iitzdany@gmail.com>
2026-04-22 21:22:21 +0200

    Update util.js

--- js/util.js
+++ js/util.js
@@ -1,4 +1,3 @@
-// https://stackoverflow.com/questions/3452546/how-do-i-get-the-youtube-video-id-from-a-url
 export function getYoutubeIdFromUrl(url) {
     return url.match(
         /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/,
@@ -10,9 +9,9 @@ export function embed(video) {
 }
 
 export function localize(num) {
-    return num.toLocaleString(undefined, { 
-        minimumFractionDigits: 0, 
-        maximumFractionDigits: 0 
+    return num.toLocaleString(undefined, {
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
     });
 }
 
@@ -46,17 +45,14 @@ export function parseRecordPercent(value) {
     return Number.isFinite(normalized) ? normalized : null;
 }
 
-// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 export function shuffle(array) {
-    let currentIndex = array.length, randomIndex;
+    let currentIndex = array.length;
+    let randomIndex;
 
-    // While there remain elements to shuffle.
     while (currentIndex != 0) {
-        // Pick a remaining element.
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
 
-        // And swap it with the current element.
         [array[currentIndex], array[randomIndex]] = [
             array[randomIndex],
             array[currentIndex],
