f1b3fe54fabee4df1ab8e19034cba75dff3ed157
pijeon <iitzdany@gmail.com>
2026-09-26 15:55:02 +0200

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -43,9 +43,6 @@ export default {
                         >
                             <span class="board-rank-wrap">
                                 <span class="board-rank" :class="'rank-' + (i + 1)">#{{ i + 1 }}</span>
-                                <span class="board-rank-sparks" aria-hidden="true">
-                                    <span v-for="spark in sparkCount(i + 1)" :key="spark" class="board-rank-spark" :class="'spark-' + spark"></span>
-                                </span>
                             </span>
                             <span class="board-name type-label-lg">{{ ientry.user }}</span>
                             <span class="board-total">{{ localize(ientry.total) }}</span>
@@ -192,11 +189,6 @@ export default {
             };
 
             return titles[rank] ?? 'Rank';
-        },
-        sparkCount(rank) {
-            if (rank <= 3) return 3;
-            if (rank <= 5) return 2;
-            return 1;
         },
         selectPlayer(index) {
             this.closing = false;
